16a10c80cc96c1fead59a26d29f31fdd0b963d8b
sailokesh23 <147254416+sailokesh23@users.noreply.github.com>
2024-07-16 20:21:39 +0530

    changed logic to util.js

--- Top_products/utils.js
+++ Top_products/utils.js
@@ -2,20 +2,18 @@ const makeId = (item) => {
   return `${item.company}-${item.id}`;
 };
 
-const sortItems = (items, key, dir) => {
-  return items.sort((a, b) => {
-    if (dir === 'asc') {
-      return a[key] > b[key] ? 1 : -1;
-    } else {
-      return a[key] < b[key] ? 1 : -1;
-    }
-  });
-};
-
 const paginate = (items, size, page) => {
   const start = (page - 1) * size;
   const end = start + size;
   return items.slice(start, end);
 };
-
+const sortItems = (items, key, dir) => {
+  return items.sort((first, second) => {
+    if (dir === 'asc') {
+      return first[key] > second[key] ? 1 : -1;
+    } else {
+      return first[key] < second[key] ? 1 : -1;
+    }
+  });
+};
 module.exports = { makeId, sortItems, paginate };
